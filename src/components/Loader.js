7ed43930a0c16b48d62d943da1e85ac10fbf75da
Faridun90 <faridun.mirzoev@gmail.com>
2021-08-29 18:54:30 +0500

    Initial commit

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Loader =()=> (
+    <div className = "text-center" >
+<div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+    
+    </div>
+)
